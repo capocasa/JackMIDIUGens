61d2222058800097504dc235089b8b7d226d18b8
Carlo Capocasa <carlo@capocasa.net>
2017-06-26 23:36:57 +0200

    Revert "prove threaded nature"
    This reverts commit 1b7965126d44b77ddbdb8fa2d0b816ea03c1fd87.

--- JackMIDIUGens.cpp
+++ JackMIDIUGens.cpp
@@ -8,7 +8,16 @@ static InterfaceTable *ft;
 
 struct JackMIDIIn: public Unit
 {
-  uint32              event_count;
+  uint32 jack_frame;
+  void* port_buf;
+  jack_nframes_t              next_event_time;
+  jack_midi_event_t           next_event;
+  jack_nframes_t              max_event_index;
+  jack_nframes_t              event_count;
+  jack_nframes_t              event_index;
+  jack_midi_event_t           event;
+  jack_transport_state_t      transport;
+  jack_position_t             position;
 };
 
 static void JackMIDIIn_next(JackMIDIIn *unit, int inNumSamples);
@@ -20,19 +29,28 @@ jack_port_t*    outputPort;
 
 int process(jack_nframes_t nframes, void *arg) {
   jack_midi_event_t event;
-  void* port_buf = jack_port_get_buffer( inputPort, nframes);
-  JackMIDIIn *unit = (JackMIDIIn*) arg;
-  jack_nframes_t event_count = jack_midi_get_event_count(port_buf);
+  jack_nframes_t event_index = 0;
+  jack_position_t         position;
 
-  if (event_count > 0) {
+  void* port_buf = jack_port_get_buffer( inputPort, nframes);
+
+  JackMIDIIn *unit = (JackMIDIIn*) arg;
+  
+  unit->port_buf = port_buf;
+  unit->jack_frame = 0;
+  unit->event_count = jack_midi_get_event_count(port_buf);
+  unit->transport = jack_transport_query( client, &position );
+
+  if (unit->event_count > 0) {
     jack_midi_event_get(&event, port_buf, 0);
-    
-    for (int i = 0; i < unit->event_count; i++) {
-      jack_midi_event_get(&event, port_buf, i);
-      //std::cout << "process " << i << " " << event.time << std::endl;
-    }
+    unit->event = event;
+    unit->event_index = 0;
+  
+for (int i = 0; i < unit->event_count; i++) { jack_midi_event_get(&event, port_buf, i); std::cout << "process " << i << " " << event.time << std::endl;  }
+  
   }
-  std::cout << "process" << std::endl;
+  unit->event_index = 0;
+  unit->max_event_index = 0;
 
   return 0;
 }
@@ -61,8 +79,7 @@ void JackMIDIIn_Ctor(JackMIDIIn* unit)
   // ar ctor 48000   2.08333e-05   64   750    0.00133333    48000    64 
   // kr ctor   750   0.00133333     1   750    0.00133333    48000    64 
   //std::cout << "ctor " << SAMPLERATE << " " << SAMPLEDUR << " " << BUFLENGTH << " " << BUFRATE << " " << BUFDUR << " " << FULLRATE << " " << FULLBUFLENGTH << " " << std::endl;   
-
-  unit->event_count = 0;
+  
   
   SETCALC(JackMIDIIn_next);
   JackMIDIIn_next(unit, 1);
@@ -70,10 +87,28 @@ void JackMIDIIn_Ctor(JackMIDIIn* unit)
 
 void JackMIDIIn_next(JackMIDIIn *unit, int inNumSamples)
 {
-  unit->event_count++;
-
-  std::cout << "next " << unit->event_count << std::endl;
   int numOutputs = unit->mNumOutputs;
+  //std::cout << "next" << std::endl;
+
+  jack_nframes_t event_count = unit->event_count;
+  jack_nframes_t event_index = unit->event_index;
+  jack_nframes_t max_event_index = unit->max_event_index;
+  jack_midi_event_t event = unit->event;
+  jack_position_t position = unit->position;
+  max_event_index += FULLBUFLENGTH;
+
+  while (event_index < event_count && event_index < max_event_index) {
+    //std::cout << "Frame " << position.frame << "  Event: " << event_index << " SubFrame#: " << event.time << " \tMessage:\t"
+    //          << (long)event.buffer[0] << "\t" << (long)event.buffer[1]
+    //          << "\t" << (long)event.buffer[2] << std::endl;
+    std::cout << "next " << event_index << " " << event.time << std::endl;
+    event_index++;
+    jack_midi_event_get(&event, unit->port_buf, event_index);
+  }
+
+  unit->event=event;
+  unit->event_index = event_index;
+
   for (int i = 0; i < inNumSamples; i++) {
     for (int j = 0; j < numOutputs; j++) {
       OUT(j)[i] = 0;
